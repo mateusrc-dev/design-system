bfd7825fcf63993087018413daaadc0edb921a27
Mateus Carvalho <mateus_raimundo95@outlook.com>
2023-01-28 22:36:47 -0300

    update design-system

---
+++ packages/docs/src/components/ColorsGrid.tsx
@@ -0,0 +1,24 @@
+// vamos criar aqui uma estrutura visual para mostrar as cores do nosso projeto
+
+import { colors } from '@ignite-ui/tokens'
+import { getContrast } from 'polished' // nos permite trabalhar com contrastes
+
+export function ColorsGrid() {
+  return Object.entries(colors).map(([key, color]) => {
+    return (
+      <div key={key} style={{ backgroundColor: color, padding: '2rem' }}>
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'spance-between',
+            fontFamily: 'monospace',
+            color: getContrast(color, '#fff') < 3.5 ? '#000' : '#fff', // se o contrates for menos que 3.5, a cor da fonte será preta
+          }}
+        >
+          <strong>${key}</strong>
+          <span>{color}</span>
+        </div>
+      </div>
+    )
+  }) // Object.entries retorna um array com vários arrays dentro onde a primeira posição do array é a chave e a segunda é o valor da chave, vamos desestrurar esse array
+}
--- packages/docs/src/stories/Button.stories.tsx
+++ packages/docs/src/stories/Button.stories.tsx
@@ -1,20 +1,22 @@
 import type { StoryObj, Meta } from '@storybook/react' // são tipagens do typescript
 import { Button, ButtonProps } from '@ignite-ui/react'
 
-export default { // definindo configuração global do botão - tudo que temos aqui dentro do objeto principal vai aparecer em todas as variações desse botão
-  args: { // argumentos
+export default {
+  // definindo configuração global do botão - tudo que temos aqui dentro do objeto principal vai aparecer em todas as variações desse botão
+  args: {
+    // argumentos
     children: 'Enviar',
   },
   title: 'Button',
   component: Button,
 } as Meta<ButtonProps>
 
-export const Primary: StoryObj<ButtonProps> = { // vamos ter variações do botão
- 
+export const Primary: StoryObj<ButtonProps> = {
+  // vamos ter variações do botão
 }
 
 export const Big: StoryObj<ButtonProps> = {
   args: {
     size: 'big', // se não for passado size colocamos como default small
-  }
+  },
 }
